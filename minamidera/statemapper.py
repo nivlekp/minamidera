e5cd557bdf923b9d1132b41789bfb03f044cf765
nivlekp <osamupang@gmail.com>
2024-08-04 22:53:13 +1000

    adds a statemapper

---
+++ minamidera/statemapper.py
@@ -0,0 +1,16 @@
+from collections.abc import Iterable
+
+import numpy.typing as npt
+
+import pang
+
+
+def map_state_sequence(state_sequence: Iterable[npt.NDArray]) -> pang.Sequence:
+    sequence = pang.Sequence()
+    for state in state_sequence:
+        sequence.extend(map_state(state))
+    return sequence
+
+
+def map_state(state: npt.NDArray) -> pang.Sequence:
+    raise NotImplementedError
