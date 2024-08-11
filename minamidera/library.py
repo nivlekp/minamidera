ce863caac17a9e285e571c0b6dd94dd1bd2fca84
nivlekp <osamupang@gmail.com>
2024-08-11 22:41:14 +1000

    some mypy stuff

--- minamidera/library.py
+++ minamidera/library.py
@@ -1,10 +1,10 @@
 import dataclasses
 import enum
 
-AXIS = enum.Enum("Axis", ["FREQUENCY", "INTENSITY", "DENSITY"])
-FREQUENCY_REGIONS = enum.Enum("FrequencyRegions", ["F0", "F1"])
-INTENSITY_REGIONS = enum.Enum("IntensityRegions", ["G0", "G1"])
-DENSITY_REGIONS = enum.Enum("DensityRegions", ["D0", "D1"])
+AXIS = enum.Enum("AXIS", ["FREQUENCY", "INTENSITY", "DENSITY"])
+FREQUENCY_REGIONS = enum.Enum("FREQUENCY_REGIONS", ["F0", "F1"])
+INTENSITY_REGIONS = enum.Enum("INTENSITY_REGIONS", ["G0", "G1"])
+DENSITY_REGIONS = enum.Enum("DENSITY_REGIONS", ["D0", "D1"])
 
 
 @dataclasses.dataclass
