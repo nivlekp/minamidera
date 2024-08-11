23d234e8320c1174313cf7115b28fea58bb337ad
nivlekp <osamupang@gmail.com>
2024-08-11 22:30:17 +1000

    adds mypy check

--- minamidera/statemapper.py
+++ minamidera/statemapper.py
@@ -5,7 +5,7 @@ import pang
 
 
 def map_state_sequence(state_sequence: Iterable[npt.NDArray]) -> pang.Sequence:
-    sequence = pang.Sequence()
+    sequence = pang.Sequence([], 0)
     for state in state_sequence:
         sequence.extend(map_state(state))
     return sequence
