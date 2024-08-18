4d80c4005a90d010c6266d6b32ed7fe191e2d959
nivlekp <osamupang@gmail.com>
2024-08-18 21:57:09 +1000

    adds another assertion

--- tests/test_soundpointgenerators.py
+++ tests/test_soundpointgenerators.py
@@ -1,10 +1,10 @@
 from minamidera import soundpointsgenerators
 
 
-def test_generated_sound_points_list_is_not_empty():
-    assert (
-        soundpointsgenerators.AtaxicSoundPointsGenerator(
-            {0, 1, 2}, {0}, {1}, {1}, 2984756
-        )(10)
-        != []
-    )
+def test_generating_ataxic_sound_points():
+    sound_points = soundpointsgenerators.AtaxicSoundPointsGenerator(
+        {0, 1, 2}, {0}, {1}, {1}, 2984756
+    )(10)
+    assert sound_points != []
+    instances = [sound_point.instance for sound_point in sound_points]
+    assert instances == sorted(instances)
