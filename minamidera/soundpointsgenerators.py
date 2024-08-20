f03cb08896e6a9a06530b6e7603a6948b7843c31
Tsz Kiu Pang <38950857+nivlekp@users.noreply.github.com>
2024-08-20 21:27:31 +1000

    Map sound points generators to sequence (#3)
    * bumps version
    
    * writes a unit test a makes it passes
    
    * fixes imports
    
    * adds another assertion
    
    * another assertion
    
    * adds a seed
    
    * another unit test
    
    * adds another unit test

--- minamidera/soundpointsgenerators.py
+++ minamidera/soundpointsgenerators.py
@@ -1,9 +1,43 @@
+import numpy as np
 import pang
 
 
 class AtaxicSoundPointsGenerator(pang.SoundPointsGenerator):
-    def __init__(self, pitches_set, intensity_set, density_set, duration_set):
-        self.pitches_set = pitches_set
+    def __init__(self, pitches_set, intensity_set, density_set, duration_set, seed):
+        self.pitches_set = np.array(tuple(pitches_set), dtype="O")
         self.intensity_set = intensity_set
         self.density_set = density_set
         self.duration_set = duration_set
+        self._random_number_generator = np.random.default_rng(seed)
+
+    def __call__(self, sequence_duration):
+        instances = self._generate_instances(sequence_duration)
+        durations = self._generate_durations(len(instances))
+        pitches = self._generate_pitches(len(instances))
+        return [
+            pang.SoundPoint(instance, duration, pitch)
+            for instance, duration, pitch in zip(
+                instances,
+                durations,
+                pitches,
+            )
+        ]
+
+    def _generate_instances(self, sequence_duration):
+        (density,) = self.density_set
+        return sorted(
+            self._random_number_generator.uniform(
+                0.0, sequence_duration, round(density * sequence_duration)
+            )
+        )
+
+    def _generate_durations(self, number_of_sound_points):
+        (duration,) = self.duration_set
+        return self._random_number_generator.exponential(
+            duration, number_of_sound_points
+        )
+
+    def _generate_pitches(self, number_of_sound_points):
+        return self._random_number_generator.choice(
+            self.pitches_set, number_of_sound_points
+        ).tolist()
--- tests/test_statemapper.py
+++ tests/test_statemapper.py
@@ -6,28 +6,40 @@ from minamidera import library, statemapper
 
 
 def test_mapping_empty_sequence():
-    assert statemapper.map_state_sequence([]) == pang.Sequence.empty_sequence()
+    assert statemapper.map_state_sequence([], 10, 0) == pang.Sequence.empty_sequence()
+
+
+def test_mapping_one_state_to_sequence():
+    sequence = statemapper.map_state_sequence([np.array([0, 1, 0, 1])], 10, 99999)
+    assert sequence.sequence_duration == 10
+
+
+def test_mapping_100_states_to_sequence():
+    sequence = statemapper.map_state_sequence([np.array([0, 1, 0, 1])] * 100, 10, 99999)
+    assert sequence.sequence_duration == 1000
 
 
 def test_mapping_sound_point_generator_from_invalid_state_vector_length():
     with pytest.raises(ValueError) as exception_info:
         statemapper.map_state_vector_to_sound_points_generator(
-            np.array([0, 0, 1, 0, 0])
+            np.array([0, 0, 1, 0, 0]), 982347
         )
     assert "does not match" in str(exception_info)
 
 
 def test_mapping_sound_point_generator_from_invalid_state_vector_value():
     with pytest.raises(ValueError) as exception_info:
-        statemapper.map_state_vector_to_sound_points_generator(np.array([0, 1, 0, 2]))
+        statemapper.map_state_vector_to_sound_points_generator(
+            np.array([0, 1, 0, 2]), 97329874
+        )
     assert "contains value other than 0 and 1" in str(exception_info)
 
 
 def test_mapping_state_vector_to_sound_point_generator():
     sound_points_generator = statemapper.map_state_vector_to_sound_points_generator(
-        np.array([0, 1, 0, 1])
+        np.array([0, 1, 0, 1]), 9273894
     )
-    assert sound_points_generator.pitches_set == library.PITCHES_SETS[0]
+    assert sound_points_generator.pitches_set.tolist() == list(library.PITCHES_SETS[0])
     assert sound_points_generator.intensity_set == library.INTENSITY_SETS[1]
     assert sound_points_generator.density_set == library.DENSITY_SETS[0]
     assert sound_points_generator.duration_set == library.DURATION_SETS[1]
