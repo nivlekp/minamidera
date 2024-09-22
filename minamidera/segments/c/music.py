698db751b9a15f40789e0dbccfc9407be416b7d4
nivlekp <osamupang@gmail.com>
2024-09-22 11:36:13 +1000

    creates two more segments

---
+++ minamidera/segments/c/music.py
@@ -0,0 +1,60 @@
+import fractions
+
+import abjad
+import numpy as np
+import pang
+from abjadext import nauert
+
+from minamidera import library, statemapper, statetransition
+from minamidera.soundpointsgenerators import SoundPointsGeneratorFactory
+
+
+def main() -> None:
+    score = library.make_empty_score()
+    sequence = statemapper.map_state_sequence(
+        statetransition.generate_flatten_state_sequences(
+            tuple([[1, 1, 1, 1]] * 10), 4, np.random.default_rng(2347856294359869466)
+        ),
+        5,
+        SoundPointsGeneratorFactory(0.2),
+        9426846856387526432,
+    )
+    search_tree = nauert.UnweightedSearchTree(
+        definition={
+            2: {2: None, 3: None},
+            3: {2: None},
+            5: None,
+        }
+    )
+    tempo = abjad.MetronomeMark(
+        abjad.Duration(1, 4), fractions.Fraction(78), decimal=True
+    )
+    q_schema = nauert.MeasurewiseQSchema(
+        search_tree=search_tree, tempo=tempo, time_signature=(4, 4)
+    )
+    quantizing_metadata = pang.populate_voices_from_sequence(
+        sequence,
+        (
+            pang.VoiceSpecification(
+                score[library.PIANO_MUSIC_VOICE_0_NAME],
+                note_server=library.TrebleNoteServer(),
+                q_schema=q_schema,
+                grace_handler=nauert.DiscardingGraceHandler(),
+            ),
+            pang.VoiceSpecification(
+                score[library.PIANO_MUSIC_VOICE_1_NAME],
+                note_server=library.BassNoteServer(),
+                q_schema=q_schema,
+                grace_handler=nauert.DiscardingGraceHandler(),
+            ),
+        ),
+    )
+    metadata = pang.build.collect_metadata(score, quantizing_metadata)
+    abjad.attach(
+        abjad.Clef("bass"), abjad.get.leaf(score[library.PIANO_MUSIC_VOICE_1_NAME], 0)
+    )
+    pang.build.persist(score, metadata)
+
+
+if __name__ == "__main__":
+    main()
